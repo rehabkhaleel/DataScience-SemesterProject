07deddfc0c8f18cbd6ba5c8137d2a1b67f43e1ac
rehabkhaleel <rehabkhaleel716@gmail.com>
2024-12-30 20:47:45 +0500

    final routing done.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import ServicesPage from "./Pages/Services";
+import  Dashboard  from "./Pages/DashBoard";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <ServicesPage/>
+    <App />
   </React.StrictMode>
 );
